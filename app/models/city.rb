da8cd31b2d1b81167a758c2f2e88beb5c54f5c7d
Dewey <hammersdrichard@gmail.com>
2021-04-29 13:58:39 -0700

    commit tree 9b5315853ec83ac7140107faeb38a80dfe6e0905

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,21 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+  def city_openings(date1, date2)
+    date1 = date1.split('-')
+    date2 = date2.split('-')
+    start = DateTime.new(date1[0].to_i, date1[1].to_i, date1[2].to_i)
+    stop = DateTime.new(date2[0].to_i, date2[1].to_i, date2[2].to_i)
+
+    listing_arr = []
+
+    self.listings.all.each do |listing|
+      listing.reservations.all.each do |reservation|
+        listing_arr << listing unless (start..stop).cover? reservation.checkin
+      end
+    end
+
+    listing_arr
+  end
 end
 
